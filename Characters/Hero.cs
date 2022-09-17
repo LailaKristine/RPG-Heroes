b0ca10d3cb43ab9a5734c23a838541e5ec222b09
LailaKristine <laila.kristine@hotmail.no>
2022-09-17 16:38:06 +0200

    Error Messages
    ErrorMessages made for armor, weapon and Required level

--- Characters/Hero.cs
+++ Characters/Hero.cs
@@ -9,19 +9,29 @@ namespace RPG_Heroes.Characters
         public int Level { get; set; }
         public HeroAttribute LevelAttributes { get; set; }
         public Dictionary<ISlot, Item> Equipments { get; set; }
-        public List<Weapon> ValidWeaponTypes { get; set; }
-        public List<Armor> ValidArmorTypes { get; set; }
+        public List<WeaponType> ValidWeaponTypes { get; set; }
+        public List<ArmorType> ValidArmorTypes { get; set; }
 
         public Hero(string name, int strength, int dexterity, int intelligence)
         {
             Name = name;
             Level = 1;
+            Equipments = new Dictionary<ISlot, Item>();
+            ValidWeaponTypes = new List<WeaponType>();
+            ValidArmorTypes = new List<ArmorType>();
+
             Equipments = new Dictionary<ISlot, Item>()
             {
                 {ISlot.Weapon, null! },
                 {ISlot.Body, null! },
                 {ISlot.Legs, null! },
                 {ISlot.Head, null! }
+            };
+            LevelAttributes = new HeroAttribute()
+            {
+                Strength = strength,
+                Dexterity = dexterity,
+                Intelligence = intelligence
             };
 
         }
